a85dfcaa311520e5693291d3385bc9aae3601551
Chris Laughlin <bluntm@gmail.com>
2016-02-22 21:57:02 +0000

    adding basic console api support

--- src/js/logger.js
+++ src/js/logger.js
@@ -1,1 +1,53 @@
-console.log('It\`s alive');
+let config = {};
+
+const canLog = (next) => {
+    if (console && config.on) {
+        next();
+    }
+};
+
+let Logger = {
+
+    config: (configuration) => {
+        config.on = configuration.on || false;
+        config.prettyPrint = configuration.prettyPrint || false;
+        config.async = configuration.async || false;
+    },
+
+    // Triggers a console log
+    log: (message) => {
+        canLog(() => {
+            console.log('LOG: ', message);
+        });
+    },
+
+    // Triggers a console info
+    info: (message) => {
+        canLog(() => {
+            console.info('INFO: ', message);
+        });
+    },
+
+    // Triggers a console debug
+    debug: (message) => {
+        canLog(() => {
+            console.debug('INFO: ', message);
+        });
+    },
+
+    // Triggers a console error
+    error: (message) => {
+        canLog(() => {
+            console.error('ERROR: ', message);
+        });
+    },
+
+    // Triggers a console warn
+    warn: (message) => {
+        canLog(() => {
+            console.warn('WARN: ', message);
+        });
+    }
+};
+
+export default Logger;
